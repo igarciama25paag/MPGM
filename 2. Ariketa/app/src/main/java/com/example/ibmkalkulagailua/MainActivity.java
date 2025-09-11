4384a2bbe9048023d42c55e5a2ff242e6130448c
2º MULTIPLATAFORMA <IK012982I18@H012982.NET>
2025-09-11 13:49:19 +0200

    2. Ariketa

---
+++ 2. Ariketa/app/src/main/java/com/example/ibmkalkulagailua/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.ibmkalkulagailua;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    RadioButton gizona, emakumea;
+    EditText adina, oinak, hazbeteak, pisua;
+    Button kalkulatu;
+    TextView textua;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.my_layout);
+
+        elementuakBilatu();
+
+        kalkulatu.setOnClickListener(v -> {
+            double altuera = (Double.parseDouble(oinak.getText().toString())*12 + Double.parseDouble(hazbeteak.getText().toString())) * 0.0254;
+            double bmi = Double.parseDouble(pisua.getText().toString()) / (altuera * altuera);
+            bmi = (double) Math.round(bmi * 100) /100;
+            if(bmi < 18.5) {
+                textua.setText(bmi + " - Pisu gutxiegi duzu...");
+            } else if(bmi > 25) {
+                textua.setText(bmi + " - Pisu gehiegi duzu...");
+            } else {
+                textua.setText(bmi + " - Pisu egokia duzu!");
+            }
+        });
+    }
+
+    private void elementuakBilatu() {
+        gizona = findViewById(R.id.gizona);
+        emakumea = findViewById(R.id.emakumea);
+
+        adina = findViewById(R.id.adina);
+        oinak = findViewById(R.id.oinak);
+        hazbeteak = findViewById(R.id.hazbeteak);
+        pisua = findViewById(R.id.pisua);
+
+        kalkulatu = findViewById(R.id.kalkulatu);
+        textua = findViewById(R.id.textua);
+    }
+}
