5b3e5bcc73e232c31ddbdc7b283b10ec386a70e3
2º MULTIPLATAFORMA <IK012982I18@H012982.NET>
2025-09-17 12:16:53 +0200

    6. Sekzioa

--- 4. Ariketa/app/src/main/java/com/example/jatetxea/Jatetxea.java
+++ 4. Ariketa/app/src/main/java/com/example/jatetxea/Jatetxea.java
@@ -1,8 +1,10 @@
 package com.example.jatetxea;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
@@ -10,6 +12,7 @@ import androidx.cardview.widget.CardView;
 public class Jatetxea extends AppCompatActivity {
 
     CardView card1, card2;
+    TextView email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,8 +21,14 @@ public class Jatetxea extends AppCompatActivity {
 
         card1 = findViewById(R.id.lehenengo_platerak_cardview);
         card2 = findViewById(R.id.bigarren_platerak_cardview);
+        email = findViewById(R.id.email);
 
         card1.setOnClickListener(v -> startActivity(new Intent(Jatetxea.this, LehenengoPlaterak.class)));
         card2.setOnClickListener(v -> startActivity(new Intent(Jatetxea.this, BigarrenPlaterak.class)));
+        email.setOnClickListener(v -> {
+            Intent emailToIntent = new Intent(Intent.ACTION_SENDTO);
+            emailToIntent.setData(Uri.parse("mailto:" + email.getText()));
+            startActivity(emailToIntent);
+        });
     }
 }
